fae60c14b2aa6c95c24184eb0040729f1e7cfd45
haanj <joshua.haan@ymail.com>
2016-07-18 13:47:43 -0700

    moved generator logic from back end into client logic

---
+++ dev/services/generator/index.js
@@ -0,0 +1,69 @@
+'use strict'
+module.exports = () => {
+  return [
+    {
+      title: 'Race',
+      value: require('./race.js')(),
+    },
+    {
+      title: 'Occupation',
+      value: require('./occupation.js')(),
+    },
+    {
+      title: 'Age',
+      value: require('./age.js')(),
+    },
+    {
+      title: 'Father\'s Occupation',
+      value: require('./occupation.js')(),
+    },
+    {
+      title: 'Mother\'s Occupation',
+      value: require('./occupation.js')(),
+    },
+    {
+      title: 'Hair Color',
+      value: require('./hair.js')(),
+    },
+    {
+      title: 'Eye Color',
+      value: require('./eyes.js')(),
+    },
+    {
+      title: 'Height',
+      value: require('./height.js')(),
+    },
+    {
+      title: 'Build',
+      value: require('./build.js')(),
+    },
+    {
+      title: 'Family Wealth',
+      value: require('./childhoodWealth.js')(),
+    },
+    {
+      title: 'Home City Size',
+      value: require('./homeCity.js')(),
+    },
+    {
+      title: 'Likes',
+      value: require('./interests.js')(),
+    },
+    {
+      title: 'Dislikes',
+      value: require('./interests.js')(),
+    },
+    {
+      title: 'Motives',
+      value: require('./motives.js')(),
+    },
+    {
+      title: 'Starting Quest',
+      value: require('./quests.js')()
+    }
+  ]
+
+
+
+
+}
--- dev/services/generator_service.js
+++ dev/services/generator_service.js
@@ -4,33 +4,12 @@ module.exports = function(app) {
   app.factory('GeneratorService', [function() {
     var myService = {};
 
-    var backstory = [
-      {"title":"Race","value":"goblin"}
-     ,{"title":"Occupation","value":"thief"}
-     ,{"title":"Age","value":"ancient"}
-     ,{"title":"Father's Occupation","value":"adventurer"}
-     ,{"title":"Mother's Occupation","value":"banker"}
-     ,{"title":"Hair Color","value":"light brown"}
-     ,{"title":"Eye Color","value":"dark green"}
-     ,{"title":"Height","value":"tall"}
-     ,{"title":"Build","value":"obese"}
-     ,{"title":"Family Wealth","value":"low income"}
-     ,{"title":"Home City Size","value":"metropolis"}
-     ,{"title":"Likes","value":"pilgrimage"}
-     ,{"title":"Dislikes","value":"following"}
-     ,{"title":"Motives","value":"love"}
-     ,{"title":"Starting Quest","value":"Start a rebellion"}];
+    var backstory = [];
     
     // retrieves story from service
     myService.getStory = function() {
-      console.log('get story');
-      return backstory;
-    };
-
-    // generates new story and returns. Currently doesn't do anything different than getStory
-    myService.newStory = function() {
-      return backstory;
-    };
+      return require('./generator')();
+    } 
 
     return myService;
   }]);
